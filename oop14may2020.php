26188324fe19f20846f28609bd4625688956d00d
Bladead <richwhynotme@gmail.com>
2020-05-14 15:49:29 +0700

    fix mai deo xong nan x2

--- oop14may2020.php
+++ oop14may2020.php
@@ -27,77 +27,97 @@ class User {
     
 }" -->
 <?php
-class Manager
+class Manager extends User
 {
-    protected $name;
-    protected $birth_day;
-    protected $score;
+    protected $listUsers = [];
 
-    // function __construct($name, $birth_day, $score)
-    // {
-    //     $this->name = $name;
-    //     $this->birth_day = $birth_day;
-    //     $this->score = $score;
-    // }
-    // function getName () {
-    //     return $this->name;
-    //   }
-    // function setName($name)
-    // {
-    //     $this->$name = $name;
-    // }
 
-    // function getBirthday()
-    // {
-    //     return $this->birth_day;
-    // }
-    // function setBirthday($birth_day)
-    // {
-    //     $this->$birth_day = $birth_day;
-    // }
-
-    // function getScore()
-    // {
-    //     return $this->score;
-    // }
-    // function setScore($score)
-    // {
-    //     $this->$score = $score;
-    // }
-
-    function addUser($name, $birth_day, $score)
+    public function addUsers($users)
     {
-        $this->name = $name;
-        $this->birth_day = $birth_day;
-        $this->score = $score;
-    }
-}
-
-class User extends Manager
-{
-    function __construct($name, $birth_day, $score)
-    {
-        $this->addUser($name, $birth_day, $score);
+        foreach ($users as $user) {
+            $this->_listUsers[] = new User($user[0], $user[1], $user[2]); // Ý tưởng
+        }
     }
     function showUserNormal()
     {
+        function cmp($a, $b) {
+            if ($a == $b) {
+                return 0;
+            }
+            return ($a < $b) ? -1 : 1;
+        }
+        
+        
+        // Sort and print the resulting array
+        uasort($this->listUsers[], 'cmp');
+        var_dump($listUsers);
     }
 
     function showUserHard()
     {
     }
 }
-$users[] = new User('a', '05-12-2000', '100');
-$users[] = new User('b', '05-12-2000', '100');
-$users[] = new User('c', '05-12-2000', '99');
-$users[] = new User('d', '05-12-1999', '98');
 
-function build_sorter($key) {
-    return function ($a, $b) use ($key) {
-        return strnatcmp($a[$key], $b[$key]);
-    };
+class User
+{
+    protected $name;
+    protected $birth_day;
+    protected $score;
+    // function __construct($name, $birth_day, $score)
+    // {
+    //     $this->name = $name;
+    //     $this->birth_day = $birth_day;
+    //     $this->score = $score;
+    // }
+    function getName()
+    {
+        return $this->name;
+    }
+    function setName($name)
+    {
+        $this->$name = $name;
+    }
+
+    function getBirthday()
+    {
+        return $this->birth_day;
+    }
+    function setBirthday($birth_day)
+    {
+        $this->$birth_day = $birth_day;
+    }
+
+    function getScore()
+    {
+        return $this->score;
+    }
+    function setScore($score)
+    {
+        $this->$score = $score;
+    }
 }
+// $users[] = new User('a', '05-12-2000', '100');
+// $users[] = new User('b', '05-12-2000', '100');
+// $users[] = new User('c', '05-12-2000', '99');
+// $users[] = new User('d', '05-12-1999', '98');
 
-usort($users, build_sorter('score'));
+// function build_sorter($key) {
+//     return function ($a, $b) use ($key) {
+//         return strnatcmp($a[$key], $b[$key]);
+//     };
+// }
+
+// usort($users, build_sorter('score'));
+$users = [
+    ['a', '05-12-2000', '100'],
+    ['b', '05-12-2000', '100'],
+    ['c', '05-12-2000', '99'],
+    ['d', '05-12-1999', '98']
+];
+$manager = new Manager('', '', '');
+$manager->addUsers($users);
+$manager->showUserNormal();
+$manager->showUserHard();
+
 
 ?>
