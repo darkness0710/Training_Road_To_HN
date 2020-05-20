a9a40b185b7aeab06e1638c8d4f2a778853aba90
Bladead <richwhynotme@gmail.com>
2020-05-20 12:59:58 +0700

    new commit laravel app on develop branch. done init, video 2,video 3

---
+++ lrvappl/storage/framework/views/be41fdd6cde9dda3ceabd4f06a3a7c77d9da56b2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Lrvappl</title>
+
+    </head>
+    <body>
+        <?php echo $__env->yieldContent('content'); ?>
+    </body>
+</html>
+<?php /**PATH E:\xampp\htdocs\code\lrvappl\resources\views/layouts/app.blade.php ENDPATH**/ ?>
