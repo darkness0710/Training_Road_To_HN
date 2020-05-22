a90832c01aff29f9b54c900ed2a23eea214a7cdb
Bladead <richwhynotme@gmail.com>
2020-05-22 20:52:37 +0700

    final commit 1(?) bug left

--- lrvappl/resources/views/layouts/app.blade.php
+++ lrvappl/resources/views/layouts/app.blade.php
@@ -1,21 +1,36 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>Lrvappl</title>
 
-    </head>
-    <body>
-        @include('inc.navbar')
-        <div class="container">
-            @include('inc.messages')
-            @yield('content')
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="//cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('inc.navbar')
+    <div class="container">
+        @include('inc.messages')
+        @yield('content')
+    </div>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
-    </body>
+</body>
+
 </html>
